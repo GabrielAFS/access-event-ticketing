ee0be100c9c99459aaef130b2d199bd986174589
GabrielAFS <gabriel_fsilva@outlook.com.br>
2025-03-25 00:54:33 -0300

    config: Setup apollo client

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,18 @@
 import { CheckoutProvider } from "@/hooks/useCheckout";
+import { graphqlClient } from "@/graphql";
+
+import { ApolloProvider } from "@apollo/client";
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
   return (
-    <CheckoutProvider>
-      <Stack>
-        <Stack.Screen name='index' />
-        <Stack.Screen name='details' />
-      </Stack>
-    </CheckoutProvider>
+    <ApolloProvider client={graphqlClient}>
+      <CheckoutProvider>
+        <Stack>
+          <Stack.Screen name='index' />
+          <Stack.Screen name='details' />
+        </Stack>
+      </CheckoutProvider>
+    </ApolloProvider>
   );
 }
