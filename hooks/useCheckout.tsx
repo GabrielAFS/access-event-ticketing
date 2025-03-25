0c94e0cddc24822d6791f5325d09b7c8cbf2c6c9
GabrielAFS <gabriel_fsilva@outlook.com.br>
2025-03-25 07:08:02 -0300

    fix(checkout): Adds purchasedOrder to useMemo dependency array

--- hooks/useCheckout.tsx
+++ hooks/useCheckout.tsx
@@ -44,7 +44,7 @@ export const CheckoutProvider: React.FC<CheckoutProviderProps> = ({
       setSelectedEvent,
       purchaseTickets,
     }),
-    [selectedEvent]
+    [selectedEvent, purchasedOrder]
   );
 
   return (
