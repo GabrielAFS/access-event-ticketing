2ed7240222d21fe3d6e8c7b15d6bfe2133580e19
GabrielAFS <gabriel_fsilva@outlook.com.br>
2025-03-25 19:27:35 -0300

    feat(index): Adds refreshControl

--- app/index.tsx
+++ app/index.tsx
@@ -1,17 +1,27 @@
 import { Event } from "@/types";
 import Card from "@/components/Card";
 import Loader from "@/components/Loader";
+import { Colors } from "@/constants/Colors";
 import { EVENTS_QUERY } from "@/graphql/queries";
 
-import { FlatList, Image, Text, View } from "react-native";
+import { useState } from "react";
+import { FlatList, Image, RefreshControl, Text, View } from "react-native";
 import { useQuery } from "@apollo/client";
 
 import styles from "./styles";
 
 export default function Index() {
-  const { data, loading, called } = useQuery<{ events: Event[] }>(EVENTS_QUERY);
+  const { data, loading, called, refetch } = useQuery<{ events: Event[] }>(
+    EVENTS_QUERY
+  );
+  const [refreshing, setRefreshing] = useState<boolean>(false);
 
   const renderProductItem = ({ item }: { item: Event }) => <Card item={item} />;
+  const handleRefresh = async () => {
+    setRefreshing(true);
+    await refetch();
+    setRefreshing(false);
+  };
 
   if (called && loading) return <Loader />;
 
@@ -23,6 +33,13 @@ export default function Index() {
         renderItem={renderProductItem}
         keyExtractor={(item) => item.id.toString()}
         contentContainerStyle={styles.contentContainer}
+        refreshControl={
+          <RefreshControl
+            tintColor={Colors.primary}
+            refreshing={refreshing}
+            onRefresh={handleRefresh}
+          />
+        }
         ListEmptyComponent={() => (
           <View style={styles.containerAbsoluteCenter}>
             <Image
