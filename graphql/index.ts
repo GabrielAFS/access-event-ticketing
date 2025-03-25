e03ecbd3d6a1635480cb9f20f8fb0f95328e4a18
GabrielAFS <gabriel_fsilva@outlook.com.br>
2025-03-25 08:16:59 -0300

    feat: Setup environment variables

--- graphql/index.ts
+++ graphql/index.ts
@@ -1,6 +1,10 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { Platform } from "react-native";
 
 export const graphqlClient = new ApolloClient({
-  uri: "http://localhost:3000/graphql",
+  uri:
+    Platform.OS === "android"
+      ? process.env.EXPO_PUBLIC_ANDROID_API_URL
+      : process.env.EXPO_PUBLIC_IOS_API_URL,
   cache: new InMemoryCache(),
 });
